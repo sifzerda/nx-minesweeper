7df0c52eabbf5513de7931de46bdf41229de79d7
Troygit config --global user.email <tydamon@hotmail.com>
2026-04-17 23:39:12 +1000

    removed construction banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono, Cormorant_Garamond, Space_Mono } from "next/font/goo
 import "./globals.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import UnderConstructionBanner from "../../components/Construction";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,22 +33,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${cormorant.variable} ${spaceMono.variable} antialiased`}
-    >
+    <html lang="en" className={`
+      ${geistSans.variable} 
+      ${geistMono.variable} 
+      ${cormorant.variable} 
+      ${spaceMono.variable} 
+      antialiased`}>
+
       <body className="flex flex-col">
+
         <Header />
-        <main className="flex-1">
 
-          <UnderConstructionBanner />
-
-          {children}</main>
-
-
-
+        <main className="flex-1">{children}</main>
 
         <Footer />
+        
       </body>
     </html>
   );
